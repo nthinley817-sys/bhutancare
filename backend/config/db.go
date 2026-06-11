2586ed0add372426bcdd5b42a2b4caf3640ab5b0
Thinley Norbu <nthinley817@gmail.com>
2026-06-11 20:47:38 +0000

    Support DATABASE_URL for Render PostgreSQL

--- backend/config/db.go
+++ backend/config/db.go
@@ -12,14 +12,21 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	var dsn string
+
+	// Use DATABASE_URL if available (Render provides this)
+	if dbURL := os.Getenv("DATABASE_URL"); dbURL != "" {
+		dsn = dbURL
+	} else {
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+		)
+	}
 
 	var err error
 	DB, err = sql.Open("postgres", dsn)
